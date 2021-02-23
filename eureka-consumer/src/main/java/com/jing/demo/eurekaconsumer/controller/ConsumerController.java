1e889375adacae43a100834bffb1b55109e6b412
jcwang <461812899@qq.com>
2021-02-23 16:53:14 +0800

    feat: consumer通过/client/sayHi调用provider的服务成功

--- eureka-consumer/src/main/java/com/jing/demo/eurekaconsumer/controller/ConsumerController.java
+++ eureka-consumer/src/main/java/com/jing/demo/eurekaconsumer/controller/ConsumerController.java
@@ -1,10 +1,14 @@
 package com.jing.demo.eurekaconsumer.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -17,8 +21,15 @@ import java.util.List;
 @Slf4j
 public class ConsumerController {
 
+    /**
+     * spring cloud的接口
+     */
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @Qualifier("eurekaClient")
+    @Autowired
+    private EurekaClient eurekaClient;
 
     @GetMapping("/service-list")
     public String listService() {
@@ -27,6 +38,22 @@ public class ConsumerController {
         // services = [provider, consumer]
         // 对应![](https://tva1.sinaimg.cn/large/008eGmZEgy1gnxjmdktagj31g20u0n8j.jpg)
         log.info("services = {}", services);
-        return "Hi";
+        return services.toString();
+    }
+
+    @GetMapping("/client/sayHi")
+    public String sayHi() {
+        String result = "";
+        // 调用服务端的接口
+        final List<InstanceInfo> providerList = eurekaClient.getInstancesByVipAddress("provider", false);
+        // 从服务列表中获取某个服务
+        final InstanceInfo instanceInfo = providerList.get(0);
+        if (InstanceInfo.InstanceStatus.UP.equals(instanceInfo.getStatus())) {
+            String url = "http://" + instanceInfo.getHostName() + ":" + instanceInfo.getPort() + "/getHi";
+            RestTemplate restTemplate = new RestTemplate();
+            result = restTemplate.getForObject(url, String.class);
+
+        }
+        return result;
     }
 }
