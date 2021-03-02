c4ec39cdf9c89d83314e80bdfcafb5fa1d9a79e1
jcwang <461812899@qq.com>
2021-03-02 11:42:31 +0800

    q#2021-03-02: localhost:90/hystrix监控页面加载失败 - ![监控页面加载失败](https://tva1.sinaimg.cn/large/e6c9d24egy1go5etrzltsj21n80u0n3x.jpg) - http://localhost:90/actuator/hystrix.stream 有数据

--- feign-consumer/src/main/java/com/jing/demo/feignconsumer/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/com/jing/demo/feignconsumer/FeignConsumerApplication.java
@@ -3,6 +3,7 @@ package com.jing.demo.feignconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class FeignConsumerApplication {
 
     public static void main(String[] args) {
