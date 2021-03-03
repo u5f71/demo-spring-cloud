8755a72143fa03008982f0dd8a561708cb01f036
jcwang <461812899@qq.com>
2021-03-03 11:44:22 +0800

    feat: 配置中心基本功能 - 可以从git读取配置文件 - /分支/git仓库上的配置文件名, 如http://localhost:81/fix/server-config-dev.properties;http://localhost:81/master/server-config-dev.properties

---
+++ config-server/src/main/java/com/jing/demo/configserver/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.jing.demo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心demo
+ *
+ * @author jcwang
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
